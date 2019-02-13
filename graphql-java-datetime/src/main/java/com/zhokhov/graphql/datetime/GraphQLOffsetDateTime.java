4b22cc2820a8e979e484b9acaede99aa8e96f958
Andreas <>
2019-02-13 19:23:40 +0200

    Implementation of java.time.OffsetDateTime for GraphQL

---
+++ graphql-java-datetime/src/main/java/com/zhokhov/graphql/datetime/GraphQLOffsetDateTime.java
@@ -0,0 +1,70 @@
+package com.zhokhov.graphql.datetime;
+
+import graphql.language.StringValue;
+import graphql.schema.Coercing;
+import graphql.schema.CoercingParseValueException;
+import graphql.schema.CoercingSerializeException;
+import graphql.schema.GraphQLScalarType;
+
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class GraphQLOffsetDateTime extends GraphQLScalarType {
+
+  private static final String DEFAULT_NAME = "OffsetDateTime";
+
+  public GraphQLOffsetDateTime() {
+    this(DEFAULT_NAME);
+  }
+
+  public GraphQLOffsetDateTime(final String name) {
+    super(
+        name, "A Java OffsetDateTime",
+        new Coercing<OffsetDateTime, String>() {
+          private OffsetDateTime convertImpl(Object input) {
+            if (input instanceof String) {
+              try {
+                return OffsetDateTime.parse((String) input, DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+              } catch (DateTimeParseException ignored) {
+                // nothing to-do
+              }
+            }
+            return null;
+          }
+
+          @Override
+          public String serialize(Object input) {
+            if (input instanceof OffsetDateTime) {
+              return DateTimeFormatter.ISO_OFFSET_DATE_TIME.format((OffsetDateTime) input);
+            } else {
+              OffsetDateTime result = convertImpl(input);
+              if (result == null) {
+                throw new CoercingSerializeException(
+                    "Invalid value '" + input + "' for OffsetDateTime");
+              }
+              return DateTimeFormatter.ISO_OFFSET_DATE_TIME.format(result);
+            }
+          }
+
+          @Override
+          public OffsetDateTime parseValue(Object input) {
+            OffsetDateTime result = convertImpl(input);
+            if (result == null) {
+              throw new CoercingParseValueException(
+                  "Invalid value '" + input + "' for OffsetDateTime");
+            }
+            return result;
+          }
+
+          @Override
+          public OffsetDateTime parseLiteral(Object input) {
+            if (!(input instanceof StringValue)) {
+              return null;
+            }
+            String value = ((StringValue) input).getValue();
+            return convertImpl(value);
+          }
+        });
+  }
+}
