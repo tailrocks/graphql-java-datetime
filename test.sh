1de1d673ae998fa8f7020ee2863d8699c47ab902
les murphy <les.murphy@eperformancegroup.com>
2020-08-07 17:03:28 -0600

    reinstate test.sh

---
+++ test.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+ABSOLUTE_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+cd "${ABSOLUTE_PATH}" || exit
+
+set -e
+
+printf "> \e[1;37mTesting graphql-java-datetime\e[0m\n"
+
+GIT_COMMIT_DESC=$(git log --format=%B -n 1)
+
+if [[ ${GIT_COMMIT_DESC} == *"[skip test]"* ]]; then
+  echo "Ignore tests"
+  exit 0
+fi
+
+if [[ ${SKIP_TEST} == "true" ]]; then
+  echo "Ignore tests"
+  exit 0
+fi
+
+./gradlew --stop
+
+rm -rf build .gradle
+
+export SPRING_PROFILES_ACTIVE="test"
+
+./gradlew clean test --stacktrace ${GRADLE_EXTRA_ARGS}
