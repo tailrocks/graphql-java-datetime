db2b3f321f9c65e484601acd76d00e802e4ef9a0
Alexey Zhokhov <alexey@zhokhov.com>
2021-01-20 01:36:29 +0800

    Groovy to Kotlin.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,29 +1,15 @@
-/*
- *  Copyright 2017 Alexey Zhokhov
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
+rootProject.name = "graphql-java-datetime"
 
-/**
- * @author <a href='mailto:alexey@zhokhov.com'>Alexey Zhokhov</a>
- */
-rootProject.name = projectName
+include(
+        // libraries
+        ":graphql-datetime-autoconfigure",
+        ":graphql-datetime-autoconfigure-common",
+        ":graphql-datetime-autoconfigure-webflux",
+        ":graphql-datetime-spring-boot-starter",
+        ":graphql-datetime-spring-boot-starter-webflux",
+        ":graphql-java-datetime",
 
-include ':graphql-java-datetime'
-include ':graphql-datetime-autoconfigure-common'
-include ':graphql-datetime-autoconfigure'
-include ':graphql-datetime-autoconfigure-webflux'
-include ':graphql-datetime-spring-boot-starter'
-include ':graphql-datetime-spring-boot-starter-webflux'
-include ':graphql-datetime-sample-app'
-include ':graphql-datetime-sample-app-webflux'
+        // apps
+        ":graphql-datetime-sample-app",
+        ":graphql-datetime-sample-app-webflux"
+)
