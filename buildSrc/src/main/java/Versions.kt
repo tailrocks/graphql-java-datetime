6cc60c3648ce76678248f4be83081fcca7fab09d
Alexey Zhokhov <alexey@zhokhov.com>
2021-01-19 18:19:47 +0800

    Added buildSrc.

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,25 @@
+object Versions {
+
+    // Languages
+
+    const val kotlin = "1.3.72"
+
+    // Gradle plugins
+
+    // https://plugins.gradle.org/plugin/com.adarshr.test-logger
+    const val gradleTestLoggerPlugin = "2.1.1"
+
+    // https://plugins.gradle.org/plugin/net.rdrei.android.buildtimetracker
+    const val gradleBuildTimeTrackerPlugin = "0.11.0"
+
+    // https://plugins.gradle.org/plugin/com.jfrog.bintray
+    const val gradleBintrayPlugin = "1.8.5"
+
+    // https://plugins.gradle.org/plugin/com.diffplug.spotless
+    const val gradleSpotlessPlugin = "5.9.0"
+
+    // Libraries
+
+    const val junit = "5.6.2"
+
+}
