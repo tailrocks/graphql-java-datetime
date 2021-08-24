27117c1cf2717ba91487db9a88a6c021b5f1039e
Alexey Zhokhov <alexey@zhokhov.com>
2021-08-24 22:46:51 +0800

    spotless fixes.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,8 +12,8 @@ dependencyResolutionManagement {
         mavenCentral()
         // uncomment if you need to use snapshot versions
         maven("https://oss.sonatype.org/content/repositories/snapshots")
-        //maven("https://s01.oss.sonatype.org/content/repositories/snapshots")
-        //maven("https://oss.jfrog.org/oss-snapshot-local")
+        // maven("https://s01.oss.sonatype.org/content/repositories/snapshots")
+        // maven("https://oss.jfrog.org/oss-snapshot-local")
     }
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
 }
