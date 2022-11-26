5017c8a0fef95f14ecf77d5f2002d5459d3837cb
Alexey Zhokhov <alexey@zhokhov.com>
2022-11-27 01:26:02 +0800

    Disable DGS and GraphQL Kickstart modules as these not migrated to Spring Boot 3 yet.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,17 +4,17 @@ rootProject.name = "graphql-java-datetime"
 
 include(
     // libraries
-    "graphql-datetime-dgs-autoconfigure",
-    "graphql-datetime-dgs-starter",
-    "graphql-datetime-kickstart-spring-boot-starter",
+    //"graphql-datetime-dgs-autoconfigure",
+    //"graphql-datetime-dgs-starter",
+    //"graphql-datetime-kickstart-spring-boot-starter",
     "graphql-datetime-spring-boot-autoconfigure",
     "graphql-datetime-spring-boot-common",
     "graphql-datetime-spring-boot-starter",
     "graphql-java-datetime",
 
     // samples
-    "samples:kickstart-webmvc",
-    "samples:kickstart-webflux",
+    //"samples:kickstart-webmvc",
+    //"samples:kickstart-webflux",
     "samples:spring-boot-webmvc",
-    "samples:dgs-webmvc"
+    //"samples:dgs-webmvc"
 )
