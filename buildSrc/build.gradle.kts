07a4a24b6a802929736964ef6bf0f5d932412bf2
Alexey Zhokhov <alexey@zhokhov.com>
2022-12-03 00:23:04 +0800

    Use Java 17 toolchain.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,15 +2,15 @@ plugins {
     `kotlin-dsl`
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
 dependencies {
     implementation(libs.tailrocks.maven.publish.conventions)
     implementation(libs.tailrocks.signing.conventions)
     implementation(libs.tailrocks.kotlin.conventions)
     implementation(libs.tailrocks.junit.conventions)
-}
-
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
-    }
 }
