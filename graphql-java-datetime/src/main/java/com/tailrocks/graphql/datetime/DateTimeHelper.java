5af43d9eac92a7d01e0f69f90a611e68706affee
Alexey Zhokhov <alexey@zhokhov.com>
2022-12-02 22:03:56 +0800

    Cleanup.

--- graphql-java-datetime/src/main/java/com/tailrocks/graphql/datetime/DateTimeHelper.java
+++ graphql-java-datetime/src/main/java/com/tailrocks/graphql/datetime/DateTimeHelper.java
@@ -36,45 +36,45 @@ final class DateTimeHelper {
     }
 
     // ISO_8601
-    public static String toISOString(LocalDateTime dateTime) {
+    static String toISOString(LocalDateTime dateTime) {
         Objects.requireNonNull(dateTime, "dateTime");
 
         return DateTimeFormatter.ISO_INSTANT.format(ZonedDateTime.of(dateTime, ZoneOffset.UTC));
     }
 
-    public static String toISOString(LocalTime time) {
+    static String toISOString(LocalTime time) {
         Objects.requireNonNull(time, "time");
 
         return DateTimeFormatter.ISO_LOCAL_TIME.format(time);
     }
 
-    public static String toISOString(Date date) {
+    static String toISOString(Date date) {
         Objects.requireNonNull(date, "date");
 
         return toISOString(toLocalDateTime(date));
     }
 
-    public static LocalDateTime toLocalDateTime(Date date) {
+    static LocalDateTime toLocalDateTime(Date date) {
         Objects.requireNonNull(date, "date");
 
         return date.toInstant().atZone(ZoneOffset.UTC).toLocalDateTime();
     }
 
-    public static Date toDate(LocalDateTime dateTime) {
+    static Date toDate(LocalDateTime dateTime) {
         Objects.requireNonNull(dateTime, "dateTime");
 
         return Date.from(dateTime.atZone(ZoneOffset.UTC).toInstant());
     }
 
-    public static Date createDate(int year, int month, int day) {
+    static Date createDate(int year, int month, int day) {
         return createDate(year, month, day, 0, 0, 0, 0);
     }
 
-    public static Date createDate(int year, int month, int day, int hours, int min, int sec) {
+    static Date createDate(int year, int month, int day, int hours, int min, int sec) {
         return createDate(year, month, day, hours, min, sec, 0);
     }
 
-    public static Date createDate(int year, int month, int day, int hours, int min, int sec, int millis) {
+    static Date createDate(int year, int month, int day, int hours, int min, int sec, int millis) {
         long nanos = TimeUnit.MILLISECONDS.toNanos(millis);
         LocalDateTime localDateTime = LocalDateTime.of(year, month, day, hours, min, sec, (int) nanos);
         return DateTimeHelper.toDate(localDateTime);
